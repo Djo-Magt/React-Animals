f07507e058667a5ac97ef3f597af6b88d30f0f01
Jonathan Magnet <jonathanmagnet@MBP-de-Jonathan.lan>
2024-03-04 14:30:32 +0100

    ajout des fichiers

---
+++ src/AnimalShow.js
@@ -0,0 +1,41 @@
+import './AnimalShow.css'
+import { useState } from 'react';
+import bird from './svg/bird.svg';
+import cat from './svg/cat.svg';
+import gator from './svg/gator.svg';
+import cow from './svg/cow.svg';
+import dog from './svg/dog.svg';
+import heart from './svg/heart.svg';
+import horse from './svg/horse.svg';
+
+const svgMap = {
+    bird,
+    cat,
+    gator,
+    cow,
+    dog,
+    horse
+};
+
+function AnimalShow({ type }){
+
+    const [hearts, setHearts] = useState(0)
+
+    const handleClick = () => {
+        setHearts(hearts + 1)
+    }
+
+    return (
+    <div className='animal-show' onClick={handleClick}>
+        <img className='animal' alt="animal" src={svgMap[type]}/>
+        <img 
+            className='heart'
+            alt="heart" 
+            src={[heart]}
+            style={{width: 10 + 10 * hearts + 'px' }}
+        />
+    </div>
+    );
+}
+
+export default AnimalShow;
